84127b9270a12060dc91136a59a69ed72523ad2e
dai.zhihong <1106869711@qq.com>
2022-01-21 01:39:32 +0800

    add user json

--- src/main/java/com/dai/www/QueryUser.java
+++ src/main/java/com/dai/www/QueryUser.java
@@ -1,19 +1,28 @@
 package com.dai.www;
 
 import Config.DBUtil;
+import com.alibaba.fastjson.JSON;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QueryUser extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json; charset=utf-8");
+        resp.setCharacterEncoding("UTF-8");
+
         Connection conn = null;
         Statement stmt = null;
+        List<User> users = new ArrayList<User>();
         try {
             conn = DBUtil.getConnection();
             String sql = "SELECT * FROM user_profile";
@@ -21,19 +30,26 @@ public class QueryUser extends HttpServlet {
             String SQL = "SELECT id, user_code FROM user_profile order by id limit 3 ";
             ResultSet rs = stmt.executeQuery(SQL);
             // 展开结果集数据库
-            while(rs.next()){
+            while(rs.next()) {
                 // 通过字段检索
-                int id  = rs.getInt("id");
+                int id = rs.getInt("id");
                 String user_code = rs.getString("user_code");
 
-                // 输出数据
-                System.out.print("ID: " + id + " User Code: " + user_code);
-                System.out.print("\n");
+                User user = new User();
+                user.setId(id);
+                user.setUserCode(user_code);
+                users.add(user);
             }
             // 完成后关闭
             rs.close();
             stmt.close();
             conn.close();
+
+            String userJson = JSON.toJSONString(users);
+            OutputStream out = resp.getOutputStream();
+            out.write(userJson.getBytes(StandardCharsets.UTF_8));
+            out.flush();
+            out.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ src/main/java/com/dai/www/User.java
@@ -0,0 +1,34 @@
+package com.dai.www;
+
+public class User {
+    private int id;
+    private String userCode;
+
+    public User() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userCode='" + userCode + '\'' +
+                '}';
+    }
+}
